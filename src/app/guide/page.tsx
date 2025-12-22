c85cd4246b05c9746a4032d083afd9aafbe688b1
Park Yong Ha <yonghapk@gmail.com>
2025-12-22 22:14:18 +0900

    add

--- src/app/guide/page.tsx
+++ src/app/guide/page.tsx
@@ -3,12 +3,35 @@ import { Button } from "@/components/button";
 
 export default function Home() {
   return (
-    <div className="p-10">
-      <div className="flex flex-wrap items-center gap-2 md:flex-row">
-        <Button size="small">Small</Button>
-        <Button>Default</Button>
-        <Button size="large">Large</Button>
-      </div>
-    </div>
+    <section className="p-10">
+      {/* Button */}
+      <article>
+        <h2 className="text-2xl font-bold mb-4">Button</h2>
+        <div className="flex flex-wrap items-center gap-2 md:flex-row">
+          <Button variant="primary" size="small">
+            Small
+          </Button>
+          <Button>Default</Button>
+          <Button size="large">Large</Button>
+          <Button variant="secondary" size="large">
+            Large
+          </Button>
+        </div>
+      </article>
+      {/* Input */}
+      <article className="mt-10">
+        <h2 className="text-2xl font-bold mb-4">Input</h2>
+        <div className="flex flex-wrap items-center gap-2 md:flex-row">
+          <Button variant="primary" size="small">
+            Small
+          </Button>
+          <Button>Default</Button>
+          <Button size="large">Large</Button>
+          <Button variant="secondary" size="large">
+            Large
+          </Button>
+        </div>
+      </article>
+    </section>
   );
 }
